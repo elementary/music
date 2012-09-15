67a0982eded8ddf68d6198829b344e13240251b8
Victor Eduardo <victoreduardm@gmail.com>
2012-09-15 17:39:54 -0600

    Update the install location of some icons, as required by the new Icon API. Also use ${PREFIX}/share/noise/icons instead of ${PREFIX}/share/noise/icons/hicolor as default icon directory.

--- src/config.vala.cmake
+++ src/config.vala.cmake
@@ -1,16 +1,18 @@
 namespace Build {
-	public const string DATADIR = "@DATADIR@";
-	public const string PKG_DATADIR = "@PKG_DATADIR@";
-	public const string ICON_DIR = "@ICON_DIR@";
-	public const string PLUGIN_DIR = "@PLUGIN_DIR@";
-	public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
-	public const string RELEASE_NAME = "@RELEASE_NAME@";
-	public const string VERSION = "@VERSION@";
-	public const string VERSION_INFO = "@VERSION_INFO@";
-	public const string CMAKE_INSTALL_PREFIX = "@CMAKE_INSTALL_PREFIX@";
-	/* Translatable launcher (.desktop) strings to be added to   */
-	/* template (.pot) file. These strings should reflect any    */
-        /* changes in these launcher keys in .desktop file.          */
-	public const string COMMENT = N_("Listen to music");
-	public const string GENERIC = N_("Music Player");
+    public const string DATADIR = "@DATADIR@";
+    public const string PKG_DATADIR = "@PKG_DATADIR@";
+    public const string PLUGIN_DIR = "@PLUGIN_DIR@";
+    public const string GETTEXT_PACKAGE = "@GETTEXT_PACKAGE@";
+    public const string RELEASE_NAME = "@RELEASE_NAME@";
+    public const string VERSION = "@VERSION@";
+    public const string VERSION_INFO = "@VERSION_INFO@";
+    public const string CMAKE_INSTALL_PREFIX = "@CMAKE_INSTALL_PREFIX@";
+    public const string ICON_DIR = "@ICON_DIR@";
+
+    /**
+     * Translatable launcher (.desktop) strings to be added to template (.pot) file.
+     * These strings should reflect any changes in these launcher keys in .desktop file
+     */
+    public const string COMMENT = N_("Listen to music");
+    public const string GENERIC = N_("Music Player");
 }
