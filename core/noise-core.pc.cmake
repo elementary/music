f38d26d5ce5ded1ba5e946d68a40916eebcbc0ca
Corentin Noël <tintou@mailoo.org>
2013-06-02 22:07:16 +0200

    Noise can now have third-party plugins

---
+++ core/noise-core.pc.cmake
@@ -0,0 +1,12 @@
+prefix=@PREFIX@
+exec_prefix=@DOLLAR@{prefix}
+libdir=@DOLLAR@{prefix}/lib
+includedir=@DOLLAR@{prefix}/include/
+ 
+Name: Noise
+Description: Noise headers  
+Version: 0.2  
+Libs: -lnoise-core
+Cflags: -I@DOLLAR@{includedir}/noise
+Requires: glib-2.0 gio-2.0 gee-1.0 libpeas-1.0 libpeas-gtk-1.0 gtk+-3.0 granite gstreamer-0.10 gstreamer-interfaces-0.10 gstreamer-tag-0.10 gstreamer-pbutils-0.10
+
