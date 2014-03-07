cb7b950c3edc94cda3e329a09681f0e466159a13
Corentin Noël <tintou@mailoo.org>
2014-03-07 16:40:39 +0100

    Fixed .deps and .pc file

--- core/noise-core.pc.cmake
+++ core/noise-core.pc.cmake
@@ -2,11 +2,11 @@ prefix=@PREFIX@
 exec_prefix=@DOLLAR@{prefix}
 libdir=@DOLLAR@{prefix}/lib
 includedir=@DOLLAR@{prefix}/include/
+plugindir=@DOLLAR@{prefix}/@PLUGIN_DIR_UNPREFIXED@
  
 Name: Noise
 Description: Noise headers  
-Version: 0.2  
+Version: @VERSION@  
 Libs: -lnoise-core
 Cflags: -I@DOLLAR@{includedir}/noise-core
-Requires: glib-2.0 gio-2.0 gee-0.8 libpeas-1.0 libpeas-gtk-1.0 gtk+-3.0 granite gstreamer-0.10 gstreamer-interfaces-0.10 gstreamer-tag-0.10 gstreamer-pbutils-0.10
-
+Requires: glib-2.0 gio-2.0 gee-0.8 libpeas-1.0 libpeas-gtk-1.0 gtk+-3.0 granite gstreamer-1.0 gstreamer-pbutils-1.0 gstreamer-tag-1.0
